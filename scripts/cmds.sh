27eb577e575840d779ff87ab422eba05579cd36a
Radenz <radenrifqirahman@gmail.com>
2023-12-19 20:57:52 +0700

    chore: add cel shading gradient image script

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,6 +1,8 @@
 cargo run --release -- "./examples/ball.png" "./examples/ball.cel.value.png" -h 8.9412 -s 3 -v 2
 cargo run --release -- "./examples/ball.png" "./examples/ball.cel.luminance.png" -h 8.9412 -s 3 -l 2
 
+cargo run --release -- "./examples/grad.png" "./examples/grad.cel.png" -h 60 180 300 -s 3 -v 2
+
 cargo run --release -- "./examples/donut.png" "./examples/donut.cel.png" -h 63.4276 111.3507 174.7783 256.5294 291.7670 -s 4 -v 4
 cargo run --release -- "./examples/if.png" "./examples/if.cel.png" -h 28.2353 73.4118 106.8235 -s 4 -v 4
 cargo run --release -- "./examples/bakery.png" "./examples/bakery.cel.png" -h 29.4118 116.4706 162.3529 221.1765 267.0588 -s 3 -v 5
