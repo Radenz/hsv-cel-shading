bc62f6cf37081fcf4547a1c279ce82b2fac35d92
Radenz <radenrifqirahman@gmail.com>
2023-12-19 20:51:08 +0700

    chore: add run scripts

---
+++ scripts/cmds.sh
@@ -0,0 +1,7 @@
+cargo run --release -- "./examples/ball.png" "./examples/ball.cel.value.png" -h 8.9412 -s 3 -v 2
+cargo run --release -- "./examples/ball.png" "./examples/ball.cel.luminance.png" -h 8.9412 -s 3 -l 2
+
+cargo run --release -- "./examples/donut.png" "./examples/donut.cel.png" -h 63.4276 111.3507 174.7783 256.5294 291.7670 -s 4 -v 4
+cargo run --release -- "./examples/if.png" "./examples/if.cel.png" -h 28.2353 73.4118 106.8235 -s 4 -v 4
+cargo run --release -- "./examples/bakery.png" "./examples/bakery.cel.png" -h 29.4118 116.4706 162.3529 221.1765 267.0588 -s 3 -v 5
+cargo run --release -- "./examples/white-lands.png" "./examples/white-lands.cel.png" -h 94.6667 148.9608 187.9412 313.2353 -s 4 -v 5
